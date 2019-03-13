9492bea08a4a42250e95d3872586c23189ed9d82
hzxfjmc <hzxfjmc@163.com>
2019-03-13 11:26:50 +0800

    first commit

---
+++ src/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
